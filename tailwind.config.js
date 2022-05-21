0b5abd7cc5aed37a24c077f9f756e661b2ff0386
noormohammad20 <mdsaim204@gmail.com>
2022-05-22 01:16:58 +0600

    create home page and setup navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,5 +3,19 @@ module.exports = {
   theme: {
     extend: {},
   },
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#D83F87",
+          secondary: "#2A1B3D",
+          accent: "#37cdbe",
+          neutral: "#3d4451",
+          "base-100": "#ffffff",
+        },
+      },
+      "light",
+    ],
+  },
   plugins: [require("daisyui")],
 }
